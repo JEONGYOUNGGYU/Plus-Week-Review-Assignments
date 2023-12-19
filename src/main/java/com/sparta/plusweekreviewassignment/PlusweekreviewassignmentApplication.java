296e1fcd4d6683be8fb0709fd6b1569256aa205a
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-12-19 17:08:15 +0900

    DOCS : 세팅 값 추가

--- src/main/java/com/sparta/plusweekreviewassignment/PlusweekreviewassignmentApplication.java
+++ src/main/java/com/sparta/plusweekreviewassignment/PlusweekreviewassignmentApplication.java
@@ -2,8 +2,11 @@ package com.sparta.plusweekreviewassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication // (exclude = SecurityAutoConfiguration.class) //Spring Security 인증 기능 제외
+@EnableJpaAuditing  // timestamp 사용하기 위해 사용
 public class PlusweekreviewassignmentApplication {
 
   public static void main(String[] args) {
